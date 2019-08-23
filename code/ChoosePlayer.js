c922aec06e27e0d17f83644539c720aa7360cc87
Jaeseok Yoon <yjaeseok@gmail.com>
2019-08-23 10:12:28 +0900

    Update dialog

--- code/ChoosePlayer.js
+++ code/ChoosePlayer.js
@@ -1,9 +1,12 @@
+const sf = require("./libs/sf.js");
+
 module.exports.function = function choosePlayer (playerAction, chooser) {
   if (chooser.currentPlayer < chooser.numPlayer) {
     const remainPlayer = chooser.numPlayer - chooser.currentPlayer - 1;
     const selectedRate = 1 / parseFloat(remainPlayer);
     const random = Math.random();
     chooser.selected =  random <= selectedRate;
+    //chooser.chooseRatio = sf("{0:#,##0.00}", remainPlayer);
     if (!chooser.selected) {
       chooser.currentPlayer += 1;    
     }
--- code/CreateChooser.js
+++ code/CreateChooser.js
@@ -1,7 +1,10 @@
+const sf = require("./libs/sf.js");
+
 module.exports.function = function createChooser (numPlayer) {
   var chooser = {
     numPlayer: numPlayer,
     currentPlayer: 1,
+    chooseRatio: sf("{0:#,##0.00}", (1 / numPlayer * 100)),
     selected: false
   };
   return chooser;
