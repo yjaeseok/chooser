3ede8dcc92a7e8f4586135bca4bf1b8546a34b86
Jaeseok Yoon <yjaeseok@gmail.com>
2019-08-23 15:16:03 +0900

    Remove decimal point

--- code/CreateChooser.js
+++ code/CreateChooser.js
@@ -4,7 +4,7 @@ module.exports.function = function createChooser (numPlayer) {
   var chooser = {
     numPlayer: numPlayer,
     currentPlayer: 1,
-    chooseRatio: sf("{0:#,##0.00}", (1 / numPlayer * 100)),
+    chooseRatio: sf("{0:#,##0}", (1 / numPlayer * 100)),
     selected: false,
     isBixby: true
   };
