e4a9e4c0bc3a0e363388f8f4a3491c03dbc5a150
Jaeseok Yoon <yjaeseok@gmail.com>
2019-08-30 22:51:47 +0900

    Add Progress

--- code/CreateChooser.js
+++ code/CreateChooser.js
@@ -1,10 +1,15 @@
 const sf = require("./libs/sf.js");
 
 module.exports.function = function createChooser (numPlayer) {
+  var progress = '';
+  for (var i = 0; i < numPlayer; i++) {
+    progress += '□';
+  }
   var chooser = {
     numPlayer: numPlayer,
     currentPlayer: 1,
     chooseRatio: sf("{0:#,##0}", (1 / numPlayer * 100)),
+    progress: progress,
     selected: false,
     isBixby: true
   };
